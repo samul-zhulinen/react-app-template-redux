5686e84f65332adaea4d61bb4d55393718f6f180
zhulinen <zhulinen@yuantutech.com>
2020-12-16 09:46:49 +0800

    feat: config

--- src/config.ts
+++ src/config.ts
@@ -1,29 +1,37 @@
-import PACKAGE from '../package.json';
-const VERSION = PACKAGE.version;
+import PACKAGE from '../package.json'
+const VERSION = PACKAGE.version
+const currentHostname = window.location.hostname;
 //h5 domain
-const H5_DAILY_DOAMIN = 'daily.yuantutech.com';
-const H5_UAT_DOMAIN = 'uat.yuantutech.com';
-export const H5_UAT2_DOMAIN = '//api-uat2.yuantutech.com';
-const H5_ONLINE_DOMAIN = 's.yuantutech.com';
-const H5_ABTEST_DOMAIN = 'test.yuantutech.com';
+const H5_DAILY_DOAMIN = "daily.yuantutech.com";
+const H5_UAT_DOMAIN = "uat.yuantutech.com";
+export const H5_UAT2_DOMAIN = "//api-uat2.yuantutech.com";
+const H5_ONLINE_DOMAIN = "s.yuantutech.com";
+const H5_ABTEST_DOMAIN = "test.yuantutech.com";
+const H5_QD_DOMAIN = "m.guahao.jkqd.gov.cn"; //在 m.guahao.jkqd.gov.cn 域名下发送请求
+const H5_NGORK = 'ngrok.yuantutech.com'
+const H5_PRE_DOMAIN = 's-pre.yuantutech.com' // 预发环境
 
 //tms domain
-let TMS_DAILY_DOAMIN = '//daily.yuantutech.com';
-let TMS_UAT_DOMAIN = '//uat.yuantutech.com';
-let TMS_ONLINE_DOMAIN = '//s.yuantutech.com';
-let TMS_ABTEST_DOMAIN = '//test.yuantutech.com';
+let TMS_DAILY_DOAMIN = "//daily.yuantutech.com";
+let TMS_UAT_DOMAIN = "//uat.yuantutech.com";
+let TMS_ONLINE_DOMAIN = "//s.yuantutech.com";
+let TMS_ABTEST_DOMAIN = "//test.yuantutech.com";
 
 //api domain
-const API_DAILY_DOMAIN = 'api.daily.yuantutech.com';
-let API_UAT_DOMAIN = 'route-uat.yuantutech.com';
+const API_LOCAL_DOMAIN = "http://test.yuantutech.com";//本地调试域名指向
+const API_DAILY_DOMAIN = "api.daily.yuantutech.com";
+// let API_UAT_DOMAIN = "api-uat.yuantutech.com";
+let API_UAT_DOMAIN = "route-uat.yuantutech.com";
 
 //家庭医生
-let API_DOC_UAT_DOMAIN = 'jump-uat.yuantutech.com';
-let API_ONLINE_DOMAIN = 'route.yuantutech.com';
-let API_PRE_DOMAIN = 'route-pre.yuantutech.com';
-let API_DOC_ONLINE_DOMAIN = 'jump.yuantutech.com';
-const API_ABTEST_DOMAIN = 'test.yuantutech.com';
-const API_QD_DOMAIN = 'm.guahao.jkqd.gov.cn';
+// let API_DOC_UAT_DOMAIN ="cactus-uat.yuantutech.com";
+let API_DOC_UAT_DOMAIN = "jump-uat.yuantutech.com";
+let API_ONLINE_DOMAIN = "route.yuantutech.com";
+let API_PRE_DOMAIN = "route-pre.yuantutech.com";
+let API_DOC_ONLINE_DOMAIN = "jump.yuantutech.com";
+// let API_DOC_ONLINE_DOMAIN = "cactus.yuantutech.com";
+const API_ABTEST_DOMAIN = "test.yuantutech.com";
+const API_QD_DOMAIN = "m.guahao.jkqd.gov.cn";
 //京都儿童医院
 let API_HCRM_UAT_DOMAIN = 'https://dctest.yuantutech.com/hcrm';
 let API_HCRM_ONLINE_DOMAIN = 'https://hcrm.yuantutech.com/hcrm';
@@ -35,15 +43,15 @@ let API_DC_UAT_DOMAIN = 'https://dctest.yuantutech.com/guidance';
 let API_DC_ONLINE_DOMAIN = 'https://dc.yuantutech.com/guidance';
 
 //信用分
-let DC_CREDIT_ONLINE_DOMAIN = 'credit-jump.yuantutech.com/creditscore';
+let DC_CREDIT_ONLINE_DOMAIN = 'credit-jump.yuantutech.com/creditscore'
 //dc doamin
-let DC_UAT_DOMAIN = 'dctest.yuantutech.com';
-let DC_ONLINE_DOMAIN = 'dc.yuantutech.com';
-let NEW_DC_ONLINE_DOMAIN = 'search.yuantutech.com';
+let DC_UAT_DOMAIN = 'dctest.yuantutech.com'
+let DC_ONLINE_DOMAIN = 'dc.yuantutech.com'
+let NEW_DC_ONLINE_DOMAIN = 'search.yuantutech.com'
 
 //工单domain
-const TICKTE_UAT_DOMAIN = 'https://ticket-uat.yuantutech.com';
-const TICKTE_ONLINE_DOMAIN = 'https://ticket.yuantutech.com';
+const TICKTE_UAT_DOMAIN = 'https://ticket-uat.yuantutech.com'
+const TICKTE_ONLINE_DOMAIN = 'https://ticket.yuantutech.com'
 
 const AOLSEE_UAT_DOMAIN = '//aolsee-aliuat.yuantutech.com/aolsee-web';
 const AOLSEE_ONLINE_DOMAIN = '//aolsee.yuantutech.com/aolsee-web';
@@ -60,13 +68,15 @@ const QUEUE_ONLINE_DOMAIN = 'http://queue.uat.yuantutech.com';
 // 诊疗包
 const ZHENLIAOBAO_UAT_DOMAIN = '//dctest.yuantutech.com/device';
 const ZHENLIAOBAO_ONLINE_DOMAIN = '//dc.yuantutech.com/device';
+// const ZHENLIAOBAO_ONLINE_DOMAIN = '//dctest.yuantutech.com/device';
 
-const IS_DAILY = false;
-const IS_UAT = true;
-const IS_ONLINE = false;
-const IS_ABTEST = false;
-const IS_QD = false;
-const IS_PRE = false;
+const IS_DAILY = currentHostname.indexOf(H5_DAILY_DOAMIN) != -1;
+const IS_UAT = currentHostname.indexOf(H5_UAT_DOMAIN) != -1 || currentHostname.indexOf(H5_NGORK) != -1;
+// const IS_UAT = currentHostname.indexOf(H5_UAT_DOMAIN) != -1;
+const IS_ONLINE = currentHostname.indexOf(H5_ONLINE_DOMAIN) != -1;
+const IS_ABTEST = currentHostname.indexOf(H5_ABTEST_DOMAIN) != -1;
+const IS_QD = currentHostname.indexOf(H5_QD_DOMAIN) != -1;
+const IS_PRE = currentHostname.indexOf(H5_PRE_DOMAIN) != -1;
 
 let API_DOMAIN = API_ONLINE_DOMAIN;
 //家庭医生
@@ -80,26 +90,27 @@ let SEARCH_DOMAIN = SEARCH_ONLINE_DOMAIN;
 let QUEUE_DOMAIN = QUEUE_ONLINE_DOMAIN;
 let ZHENLIAOBAO_DOM = ZHENLIAOBAO_ONLINE_DOMAIN;
 //京都儿童医院
-let API_HCRM_DOMAIN = API_HCRM_ONLINE_DOMAIN;
+let API_HCRM_DOMAIN = API_HCRM_ONLINE_DOMAIN
 //京都在线商城
-let API_HCRM_SHOP_DOMAIN = API_HCRM_SHOP_ONLINE_DOMAIN;
+let API_HCRM_SHOP_DOMAIN = API_HCRM_SHOP_ONLINE_DOMAIN
 //智能导诊
-let API_DC_DOMAIN = API_DC_ONLINE_DOMAIN;
-let DC_DOMAIN = DC_ONLINE_DOMAIN;
-let NEW_DC_DOMAIN = NEW_DC_ONLINE_DOMAIN;
+let API_DC_DOMAIN = API_DC_ONLINE_DOMAIN
+let DC_DOMAIN = DC_ONLINE_DOMAIN
+let NEW_DC_DOMAIN = NEW_DC_ONLINE_DOMAIN
 //信用分
-let DC_CREDIT_DOMAIN = DC_CREDIT_ONLINE_DOMAIN;
+let DC_CREDIT_DOMAIN = DC_CREDIT_ONLINE_DOMAIN
 
 // 问诊
-const API_CONSULT_UAT = '//route-uat.yuantutech.com';
+const API_CONSULT_UAT = '//route-uat.yuantutech.com'
 
-const API_CONSULT_ONLINE = '//route-internet.yuantutech.com';
-let API_CONSULT = API_CONSULT_ONLINE;
+const API_CONSULT_ONLINE = '//route-internet.yuantutech.com'
+let API_CONSULT = API_CONSULT_ONLINE
 
 // 支付监控
-const API_PAYMONITOR_UAT = 'paymonitor-jump-uat.yuantutech.com/paymonitor';
-const API_PAYMONITOR_ONLINE = 'paymonitor-jump.yuantutech.com/paymonitor';
-let API_PAYMONITOR = API_PAYMONITOR_UAT;
+const API_PAYMONITOR_UAT = 'paymonitor-jump-uat.yuantutech.com/paymonitor'
+const API_PAYMONITOR_ONLINE = 'paymonitor-jump.yuantutech.com/paymonitor'
+let API_PAYMONITOR = API_PAYMONITOR_UAT
+
 
 if (IS_DAILY) {
   API_DOMAIN = API_DAILY_DOMAIN;
@@ -113,8 +124,8 @@ if (IS_DAILY) {
   SEARCH_DOMAIN = SEARCH_UAT_DOMAIN;
   SERVICE_DOMAIN = SERVICE_UAT_DOMAIN;
   API_DC_DOMAIN = API_DC_UAT_DOMAIN;
-  ZHENLIAOBAO_DOM = ZHENLIAOBAO_UAT_DOMAIN;
-  DC_DOMAIN = DC_UAT_DOMAIN;
+  ZHENLIAOBAO_DOM = ZHENLIAOBAO_UAT_DOMAIN
+  DC_DOMAIN = DC_UAT_DOMAIN
 } else if (IS_QD) {
   API_DOMAIN = API_QD_DOMAIN;
   H5_DOMAIN = H5_ONLINE_DOMAIN;
@@ -143,8 +154,8 @@ if (IS_DAILY) {
   SERVICE_DOMAIN = SERVICE_UAT_DOMAIN;
   QUEUE_DOMAIN = QUEUE_UAT_DOMAIN;
   ZHENLIAOBAO_DOM = ZHENLIAOBAO_UAT_DOMAIN;
-  DC_DOMAIN = DC_UAT_DOMAIN;
-  API_CONSULT = API_CONSULT_UAT;
+  DC_DOMAIN = DC_UAT_DOMAIN
+  API_CONSULT = API_CONSULT_UAT
 } else if (IS_ABTEST) {
   API_DOMAIN = API_ABTEST_DOMAIN;
   H5_DOMAIN = H5_ABTEST_DOMAIN;
@@ -156,12 +167,12 @@ if (IS_DAILY) {
   QUEUE_DOMAIN = QUEUE_UAT_DOMAIN;
 } else if (IS_PRE) {
   API_DOMAIN = API_PRE_DOMAIN;
-  API_CONSULT = '//' + API_PRE_DOMAIN;
+  API_CONSULT = '//' + API_PRE_DOMAIN
 } else {
   API_DOMAIN = API_ONLINE_DOMAIN;
   API_DOC_DOMAIN = API_DOC_ONLINE_DOMAIN;
-  API_HCRM_DOMAIN = API_HCRM_ONLINE_DOMAIN;
-  API_HCRM_SHOP_DOMAIN = API_HCRM_SHOP_ONLINE_DOMAIN;
+  API_HCRM_DOMAIN = API_HCRM_ONLINE_DOMAIN
+  API_HCRM_SHOP_DOMAIN = API_HCRM_SHOP_ONLINE_DOMAIN
   API_DC_DOMAIN = API_DC_ONLINE_DOMAIN;
   H5_DOMAIN = H5_ONLINE_DOMAIN;
   TICKET_DOMAIN = TICKTE_ONLINE_DOMAIN;
@@ -170,20 +181,22 @@ if (IS_DAILY) {
   SEARCH_DOMAIN = SEARCH_ONLINE_DOMAIN;
   SERVICE_DOMAIN = SERVICE_ONLINE_DOMAIN;
   ZHENLIAOBAO_DOM = ZHENLIAOBAO_ONLINE_DOMAIN;
-  DC_DOMAIN = DC_ONLINE_DOMAIN;
-  API_CONSULT = API_CONSULT_ONLINE;
-  DC_CREDIT_DOMAIN = DC_CREDIT_ONLINE_DOMAIN;
-  API_PAYMONITOR = API_PAYMONITOR_ONLINE;
+  DC_DOMAIN = DC_ONLINE_DOMAIN
+  API_CONSULT = API_CONSULT_ONLINE
+  DC_CREDIT_DOMAIN = DC_CREDIT_ONLINE_DOMAIN
+  API_PAYMONITOR = API_PAYMONITOR_ONLINE
 }
 
-const userWebApiDomain = localStorage.getItem('userWebApiDomain');
-const TRANSFER = `${TMS_DOMAIN}/tms/h5/transfer.php?`;
+
+const userWebApiDomain = localStorage.getItem('userWebApiDomain')
+const TRANSFER = `${TMS_DOMAIN}/tms/h5/transfer.php?`
+
 
 if (userWebApiDomain) {
-  API_DOMAIN = userWebApiDomain;
+  API_DOMAIN = userWebApiDomain
 }
 
-let PROTOCOL = 'https://';
+let PROTOCOL = 'https://'
 let config = {
   VERSION,
   IS_DAILY,
@@ -209,8 +222,8 @@ let config = {
   DC_CREDIT_DOMAIN,
   NEW_DC_DOMAIN,
   API_PAYMONITOR,
-  TRANSFER,
-};
+  TRANSFER
+}
 
 export {
   VERSION,
@@ -237,6 +250,6 @@ export {
   DC_CREDIT_DOMAIN,
   NEW_DC_DOMAIN,
   API_PAYMONITOR,
-  TRANSFER,
-};
-export default config;
+  TRANSFER
+}
+export default config
